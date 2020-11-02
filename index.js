a0633f0234e8bbb32ad126709f0c9ab12c173790
VietQuoc356 <66951449+VietQuoc356@users.noreply.github.com>
2020-11-02 10:42:30 +0000

    some files

---
+++ index.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+ 
+
+
+console.log(chalk.blue('Aya'));
+console.log(chalk.red('Carlos'));
+console.log(chalk.yellow('Viet'));
+console.log(chalk.green('Hello world!'));
